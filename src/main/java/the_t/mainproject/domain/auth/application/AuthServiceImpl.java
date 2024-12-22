bd8c25a1dd1f1c7bf0e9b5b1a421f2550ca26f5e
HuniHuns <i690367@gmail.com>
2024-12-23 00:00:11 +0900

    [REFACTOR] : 회원가입 기본코드 수정

--- src/main/java/the_t/mainproject/domain/auth/application/AuthServiceImpl.java
+++ src/main/java/the_t/mainproject/domain/auth/application/AuthServiceImpl.java
@@ -14,26 +14,19 @@ import the_t.mainproject.domain.member.domain.repository.MemberRepository;
 @Service
 public class AuthServiceImpl implements AuthService {
 
-    private MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
 
-    @Override
     @Transactional
     public ResponseEntity<?> join(JoinReq joinReq) {
-        // 이메일 중복 체크
-        if (memberRepository.existsByEmail(joinReq.getEmail())) {
-            return ResponseEntity.badRequest().body("이미 사용중인 이메일입니다.");
-        }
-
-        // 비밀번호 암호화 후 저장
         Member member = Member.builder()
                 .email(joinReq.getEmail())
-                .password(passwordEncoder.encode(joinReq.getPassword()))
                 .name(joinReq.getName())
+                .password(joinReq.getPassword())
                 .nickname(joinReq.getNickname())
                 .build();
 
         memberRepository.save(member);
-        return ResponseEntity.ok("회원가입 완료");
+
+        return ResponseEntity.ok("회원가입 성공");
     }
 }
