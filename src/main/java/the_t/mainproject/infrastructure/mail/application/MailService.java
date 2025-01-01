41ee66a382adc6606f97e6df8e7f39461f7f2cfa
HuniHuns <i690367@gmail.com>
2025-01-01 15:26:21 +0900

    [REFACTOR] : 메일 폴더 구조 변경

--- src/main/java/the_t/mainproject/infrastructure/mail/application/MailService.java
+++ src/main/java/the_t/mainproject/infrastructure/mail/application/MailService.java
@@ -1,38 +1,36 @@
 package the_t.mainproject.infrastructure.mail.application;
 
-import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import the_t.mainproject.global.common.Message;
+import the_t.mainproject.domain.member.domain.repository.MemberRepository;
 import the_t.mainproject.global.common.SuccessResponse;
+import the_t.mainproject.infrastructure.mail.MailUtil;
 import the_t.mainproject.infrastructure.mail.dto.MailCodeRes;
 import the_t.mainproject.infrastructure.redis.RedisUtil;
-
-import java.security.SecureRandom;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
 public class MailService {
 
-    private final String SUBJECT = "SpillTheTea 인증 링크입니다.";
-    private final String SENDER_NAME = "SpillTheTea";
-
-    @Value("${spring.mail.username}")
-    private String fromEmail;
+    private final String VERIFY_SUFFIX = "_verify";
 
     private final JavaMailSender mailSender;
     private final RedisUtil redisUtil;
+    private final MailUtil mailUtil;
+
+    private final MemberRepository memberRepository;
 
     public SuccessResponse<MailCodeRes> sendMail(String email) throws Exception {
-        String code = generateCode();
-        redisUtil.setDataExpire(code, String.valueOf(false), 60*3L);
-        MimeMessage mimeMessage = createMessage(code, email);
+        if(memberRepository.existsByEmail(email))
+            throw new IllegalArgumentException("이미 가입된 이메일입니다.");
+        String code = mailUtil.generateCode();
+        redisUtil.setDataExpire(code, email, 60 * 3L);
+
+        MimeMessage mimeMessage = mailUtil.createMessage(code, email);
         mailSender.send(mimeMessage);
 
         MailCodeRes mailCodeRes = MailCodeRes.builder()
@@ -42,40 +40,19 @@ public class MailService {
         return SuccessResponse.of(mailCodeRes);
     }
 
-    public SuccessResponse<Message> verifyCode(String code) {
+    public void verifyCode(String code) {
         String data = redisUtil.getData(code);
         if (data == null)
             throw new IllegalArgumentException("유효하지 않은 코드입니다.");
 
-        redisUtil.setDataExpire(code, String.valueOf(true), 60 * 60L);
-
-        Message message = Message.builder()
-                .message("이메일 인증이 완료되었습니다.")
-                .build();
-
-        return SuccessResponse.of(message);
+        redisUtil.deleteData(code);
+        redisUtil.setDataExpire(data + VERIFY_SUFFIX, String.valueOf(true), 60 * 60L);
+//
+//        Message message = Message.builder()
+//                .message("이메일 인증이 완료되었습니다.")
+//                .build();
+//
+//        return SuccessResponse.of(message);
     }
 
-    private MimeMessage createMessage(String code, String email) throws MessagingException {
-        MimeMessage mimeMessage = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
-        helper.setTo(email);
-        helper.setSubject(SUBJECT);
-        helper.setFrom(fromEmail);
-        helper.setText(code, true);
-
-        return mimeMessage;
-    }
-
-
-    // Description : 코드 생성 함수 (00000000 ~ zzzzzzzz) (8자리)
-    private String generateCode() {
-        SecureRandom random = new SecureRandom();
-        // nextLong(long bound) : 0(포함)부터 입력된 bound(미포함) 사이의 랜덤 정수를 반환
-        long randomNumber = random.nextLong(2821109907455L + 1);
-        String code = Long.toString(randomNumber, 36);
-        code = String.format("%8s", code).replace(' ', '0');
-
-        return code;
-    }
 }
--- src/main/java/the_t/mainproject/infrastructure/mail/presentation/MailController.java
+++ src/main/java/the_t/mainproject/infrastructure/mail/presentation/MailController.java
@@ -1,15 +1,17 @@
 package the_t.mainproject.infrastructure.mail.presentation;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import the_t.mainproject.global.common.Message;
 import the_t.mainproject.global.common.SuccessResponse;
 import the_t.mainproject.infrastructure.mail.application.MailService;
 import the_t.mainproject.infrastructure.mail.dto.MailCodeRes;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,7 +26,9 @@ public class MailController {
     }
 
     @GetMapping(value = "/verify")
-    public ResponseEntity<SuccessResponse<Message>> verify(@RequestParam String code) {
-        return ResponseEntity.ok(mailService.verifyCode(code));
+    public void verify(@RequestParam String code, HttpServletResponse response) throws IOException {
+        mailService.verifyCode(code);
+        String redirect_url = "http://www.google.com";
+        response.sendRedirect(redirect_url);
     }
 }
