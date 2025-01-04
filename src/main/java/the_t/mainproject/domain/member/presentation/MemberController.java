6e2b3ec862ddc47b52d1262cb27a7245a2d77a9c
HuniHuns <i690367@gmail.com>
2025-01-04 21:24:13 +0900

    [FEAT]: 회원정보변경(닉네임, 비밀번호) API

--- src/main/java/the_t/mainproject/domain/member/presentation/MemberController.java
+++ src/main/java/the_t/mainproject/domain/member/presentation/MemberController.java
@@ -4,11 +4,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import the_t.mainproject.domain.member.application.MemberServiceImpl;
+import the_t.mainproject.domain.member.dto.MemberUpdateReq;
 import the_t.mainproject.global.common.Message;
 import the_t.mainproject.global.common.SuccessResponse;
 import the_t.mainproject.global.security.UserDetailsImpl;
@@ -30,5 +28,12 @@ public class MemberController {
     @DeleteMapping("/image")
     public ResponseEntity<SuccessResponse<Message>> deleteProfileImage(@AuthenticationPrincipal UserDetailsImpl member) {
         return ResponseEntity.ok(memberService.deleteProfileImage(member));
+    }
+
+    @Operation(summary = "회원정보 수정 (닉네임, 비밀번호)")
+    @PutMapping("/update")
+    public ResponseEntity<SuccessResponse<Message>> updateNicknamePassword(@AuthenticationPrincipal UserDetailsImpl member,
+                                                                           @RequestBody MemberUpdateReq memberUpdateReq) {
+        return ResponseEntity.ok(memberService.updateNicknamePassword(member, memberUpdateReq));
     }
 }
