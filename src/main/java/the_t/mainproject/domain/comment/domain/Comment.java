022658a866891245d4cca28c5e52382751d25a3a
HuniHuns <i690367@gmail.com>
2025-01-06 08:32:26 +0900

    [FEAT]: 게시글 공감 API

--- src/main/java/the_t/mainproject/domain/comment/domain/Comment.java
+++ src/main/java/the_t/mainproject/domain/comment/domain/Comment.java
@@ -58,4 +58,8 @@ public class Comment extends BaseEntity {
         this.likedCount = 0;
         this.reportedCount = 0;
     }
+
+    public void addLikedCount() {
+        this.likedCount++;
+    }
 }
