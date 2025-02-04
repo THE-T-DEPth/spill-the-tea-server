6a122ce179207e7790a7cba7d713d7077f72a717
HuniHuns <i690367@gmail.com>
2025-02-04 12:51:15 +0900

    [CHORE]: springboot timezone 설정

--- src/main/java/the_t/mainproject/SpilltheTeaApplication.java
+++ src/main/java/the_t/mainproject/SpilltheTeaApplication.java
@@ -1,10 +1,18 @@
 package the_t.mainproject;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class SpilltheTeaApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpilltheTeaApplication.class, args);
